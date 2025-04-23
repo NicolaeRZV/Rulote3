30af5a42accd518e4b60e43356921f5f2e44b007
RazvanNicolae <122481441+NicolaeRZV@users.noreply.github.com>
2025-04-23 15:43:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function openArticles(){
+  window.location.href = "index.html";
+
+}
+function openReviews(){
+    window.location.href = "reviews.html";
+  
+  }
+  function openContact(){
+    window.location.href = "Contact.html";
+  
+  }
+  function openPackets(){
+    window.location.href = "Packet.html";
+  
+  }  
+
+  function openSolar(){
+    window.location.href = "Solar.html";
+  
+  }  
+
+  function openMover(){
+    window.location.href = "Mover.html";
+  
+  }  
+
+  
